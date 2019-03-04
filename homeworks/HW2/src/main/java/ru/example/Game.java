904ee916aa933686f28a6a5348fcd84e5e8bb5bc
venam117 <Dru.ny@mail.ru>
2019-03-04 15:25:52 +0300

    Почти добавили логгер.

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -1,8 +1,13 @@
 package ru.example;
 
+import org.slf4j.LoggerFactory;
+
 public class Game {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(Game.class);
+
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        log.info("Логгер живой");
 
     }
 
