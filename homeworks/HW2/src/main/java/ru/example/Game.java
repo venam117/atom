5b75ea22edc59a03dcb81e1c55f064ced7296022
venam117 <Dru.ny@mail.ru>
2019-03-04 14:57:06 +0300

    Добавили checkstyle.

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -1,8 +1,7 @@
 package ru.example;
 
 public class Game {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello World!!");
 
     }
