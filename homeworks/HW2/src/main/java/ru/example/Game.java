bceadb04f020ba548dc4a3ab2a54fa6840dd9e3a
venam <Dru.ny@mail.ru>
2019-03-06 19:14:11 +0300

    Подключен логгер))

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -6,8 +6,9 @@ public class Game {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(Game.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
         log.info("Логгер живой");
+        System.out.println("Hello World!!");
+
 
     }
 
