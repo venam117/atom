b19bdba82d3817975a98e797edf1ea0366d118c1
venam117 <48046203+venam117@users.noreply.github.com>
2019-03-17 18:07:04 +0300

    Update ChatClientTest.java

--- lecture04/src/test/java/ru/atom/chat/client/ChatClientTest.java
+++ lecture04/src/test/java/ru/atom/chat/client/ChatClientTest.java
@@ -13,9 +13,9 @@ import ru.atom.chat.server.ChatApplication;
 import java.io.IOException;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ChatApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class ChatClientT
-{
+@SpringBootTest(classes={ChatApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
+public class ChatClientTest {
     private static String MY_NAME_IN_CHAT = "I_AM_STUPID";
     private static String MY_MESSAGE_TO_CHAT = "SOMEONE_KILL_ME";
 
