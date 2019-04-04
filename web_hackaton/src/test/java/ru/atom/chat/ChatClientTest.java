fdafbaf9a7092f9b0bbecc269b6f2879d721a921
venam <Dru.ny@mail.ru>
2019-04-04 15:22:18 +0300

    Мой комит

--- web_hackaton/src/test/java/ru/atom/chat/ChatClientTest.java
+++ web_hackaton/src/test/java/ru/atom/chat/ChatClientTest.java
@@ -13,8 +13,7 @@ import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ChatApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class ChatClientTest
-{
+public class ChatClientTest {
     private static String MY_NAME_IN_CHAT = "I_AM_STUPID";
     private static String MY_MESSAGE_TO_CHAT = "SOMEONE_KILL_ME";
 
